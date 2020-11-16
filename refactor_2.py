70c1bff85ada31606922f989fb0a7d4c835466c7
Krisztian Hackl <info@hackl.hu>
2020-11-16 11:54:24 +0100

    Add files via upload
    Refactor v2.0

---
+++ refactor_2.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 56, 32, 51, 2, 8, 92, 15]
+print(numbers)
+
+def sort_list(_list):
+    N = len(_list)
+    for number in _list:
+        while number in range(1, N):
+            if _list[number] < _list[number - 1]:
+                temp = _list[number - 1]
+                _list[number - 1] = _list[number]
+                _list[number] = temp
+            number += 1
+    print(_list)
+
+
+sort_list(numbers)
